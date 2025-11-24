410f77a370d27afccd7dbd6cf4e5a4c6ce2f43d0
Mukund Deotale <mukundeotale9860@gmail.com>
2025-11-24 14:01:54 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,8 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket = "new-s3-bukkkbalti123" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
-    profile = "eks"
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
