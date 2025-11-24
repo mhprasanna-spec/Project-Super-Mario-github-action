ebfb671b4247603ece140f03a30d0bb8ce8f1ba7
Mukund Deotale <mukundeotale9860@gmail.com>
2025-11-24 13:54:54 +0530

    Change S3 bucket name in backend.tf
    Updated the S3 bucket name for the Terraform backend configuration.

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "super-mario-tf-kubernetes-project" # Replace with your actual S3 bucket name
+    bucket = "new-s3-bukkkbalti123" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
     profile = "eks"
